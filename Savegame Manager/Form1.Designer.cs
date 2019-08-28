92983f1be47be4986bdc984f2985acbd3b6fe625
Nacho223 <Nacho223@users.noreply.github.com>
2019-08-28 18:48:22 +0200

    First release. Tested and working

---
+++ Savegame Manager/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Savegame_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FromList = new System.Windows.Forms.TreeView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.Restore = new System.Windows.Forms.ToolStripMenuItem();
+            this.Delete = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // FromList
+            // 
+            this.FromList.Location = new System.Drawing.Point(12, 40);
+            this.FromList.Name = "FromList";
+            this.FromList.Size = new System.Drawing.Size(297, 394);
+            this.FromList.TabIndex = 3;
+            this.FromList.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.FromList_AfterSelect);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Restore,
+            this.Delete,
+            this.refreshToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(447, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Restore
+            // 
+            this.Restore.Name = "Restore";
+            this.Restore.Size = new System.Drawing.Size(58, 20);
+            this.Restore.Text = "Restore";
+            this.Restore.Click += new System.EventHandler(this.restoreToolStripMenuItem_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(52, 20);
+            this.Delete.Text = "Delete";
+            this.Delete.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // refreshToolStripMenuItem
+            // 
+            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.refreshToolStripMenuItem.Text = "Refresh";
+            this.refreshToolStripMenuItem.Click += new System.EventHandler(this.refreshToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 450);
+            this.Controls.Add(this.FromList);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "SaveGame Manager";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TreeView FromList;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TreeView ToList;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem Restore;
+        private System.Windows.Forms.ToolStripMenuItem Delete;
+        private System.Windows.Forms.ToolStripMenuItem refreshToolStripMenuItem;
+    }
+}
+
---
+++ Savegame Manager/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace Savegame_Manager
+{
+    public partial class Form1 : Form
+    {
+        System.Timers.Timer refreshTimer = new System.Timers.Timer();
+        private static readonly string path = Path.Combine(Environment.ExpandEnvironmentVariables("%userprofile%"), "Documents\\My Games\\");
+        private string selectedNode = string.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+            refreshTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            refreshTimer.Interval = 60000;
+            refreshTimer.Enabled = true;
+            PopulateTreeView();
+           // PopulateTreeViewTo();
+        }
+
+        private void OnTimedEvent(object source, EventArgs e)
+        {
+            PopulateTreeView();
+        }
+
+        private void PopulateTreeView()
+        {
+            TreeNode rootNode;
+            FromList.Nodes.Clear();
+            DirectoryInfo info = new DirectoryInfo(Path.Combine(path, "Fuck Numantian Games\\Saves\\"));
+            if (info.Exists)
+            {
+                rootNode = new TreeNode(info.Name);
+                rootNode.Tag = info;
+                GetDirectories(info.GetDirectories(), rootNode);
+                FromList.Nodes.Add(rootNode);
+            }
+            FromList.ExpandAll();
+        }
+
+        private void PopulateTreeViewTo()
+        {
+            TreeNode rootNode;
+
+            DirectoryInfo info = new DirectoryInfo(Path.Combine(Environment.ExpandEnvironmentVariables("%userprofile%"), "Documents\\"));
+            if (info.Exists)
+            {
+                rootNode = new TreeNode(info.Name);
+                rootNode.Tag = info;
+                GetDirectories(info.GetDirectories(), rootNode);
+                ToList.Nodes.Add(rootNode);
+            }
+        }
+
+        private void GetDirectories(DirectoryInfo[] subDirs, TreeNode nodeToAddTo)
+        {
+            try
+            {
+                TreeNode aNode;
+                DirectoryInfo[] subSubDirs;
+                foreach (DirectoryInfo subDir in subDirs)
+                {
+                    aNode = new TreeNode(subDir.Name, 0, 0);
+                    aNode.Tag = subDir;
+                    aNode.ImageKey = "folder";
+                    subSubDirs = subDir.GetDirectories();
+                    if (subSubDirs.Length != 0)
+                    {
+                        GetDirectories(subSubDirs, aNode);
+                    }
+                    nodeToAddTo.Nodes.Add(aNode);
+                }
+            }
+            catch (Exception) { }
+        }
+
+
+        private void FromList_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            selectedNode = e.Node.FullPath;
+            Restore.Enabled = true;
+            Delete.Enabled = true;
+        }
+
+
+        private void restoreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (selectedNode != string.Empty)
+            {
+                foreach (string file in Directory.GetFiles(Path.Combine(path, "Fuck Numantian Games\\" + selectedNode + "\\")))
+                {
+                    string[] division = file.Split('\\');
+                    string localFile = division[division.Length - 1];
+                    File.Copy(file, Path.Combine(path, "They Are Billions\\Saves\\" + localFile), true);
+                }
+                PopulateTreeView();
+            }
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (selectedNode != string.Empty)
+            {
+                foreach (string file in Directory.GetFiles(Path.GetFullPath(selectedNode)))
+                {
+                    File.Delete(file);
+                }
+                PopulateTreeView();
+            }
+        }
+
+        private void refreshToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PopulateTreeView();
+        }
+    }
+}
